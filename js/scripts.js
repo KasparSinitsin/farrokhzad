f3c0149d6fccce7e311a8be1eb010f5a5f6c18c8
Kaspar Sinitsin <kaspar.sinitsin@students.beaminstitute.org>
2025-02-17 22:49:08 +0100

    finalized version

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,1 @@
-
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
-}
-
+function menuToggle(){var x=document.getElementById('myNavtoggle');if(x.className==='navtoggle'){x.className+=' responsive';}else{x.className='navtoggle';}}
